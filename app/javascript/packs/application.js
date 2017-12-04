de50b3a89d6449b6c8e1411dad44ab6296b6ba89
Stephen MacLennan <milesmaclennan@gmail.com>
2017-12-04 18:37:48 +0200

    Add stylesheet default and variables to webpacker build path and import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,10 @@ import Axios from 'axios';
 import VueAxios from 'vue-axios';
 import Example from './components/Example.vue';
 import TurbolinksAdapter from 'vue-turbolinks';
+import StyleVariables from 'stylesheets/variables.scss';
+import StyleDefatults from 'stylesheets/defaults.scss';
 
-Vue.use(TurbolinksAdapter, Axios, VueAxios)
+Vue.use(TurbolinksAdapter, Axios, VueAxios, StyleVariables, StyleDefatults)
 
 document.addEventListener('turbolinks:load', () => {
   Axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
