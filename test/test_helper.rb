30a14317cb373b91602bbe5bda3d31cd7632ab5e
Stephen MacLennan <milesmaclennan@gmail.com>
2017-12-11 07:48:58 +0200

    Add devise sign in test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -30,3 +30,15 @@ class ActiveSupport::TestCase
   fixtures :all
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
+
+class ActionDispatch::IntegrationTest
+  def sign_in(user)
+    post user_session_path \
+      "user[email]"    => user.email,
+      "user[password]" => user.password
+  end
+end
